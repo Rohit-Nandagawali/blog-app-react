851feb7f239d3b02a3b315454ad93d488a5fb13d
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2022-11-10 18:54:18 +0530

    ui-changed
    ui changed

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css"
 const BlogList = ({blogs,title}) => {
     return ( 
         <div className="blogList">
-            <h2>{title}</h2>
+            <h2 className="title">{title}</h2>
             {blogs.map((blog)=>(
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
