10d6231fd372aa874d70f92f331aad3218a39a7f
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2022-11-10 16:10:27 +0530

    On click event
    Learn about onclick event and handle event

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,14 @@
 import "./Home.css"
 
 const Home = () => {
+
+    const handleClick=()=>{
+        console.log('i am clicked');
+    }
     return ( 
         <div className="Home">
             <h1>homepage</h1>
+            <button onClick={handleClick}>click me</button>
         </div>
      );
 }
