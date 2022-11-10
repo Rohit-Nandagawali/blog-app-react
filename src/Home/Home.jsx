07884cc75c5691c77a5fc2e2042c61fcb548a827
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2022-11-10 16:27:35 +0530

    understood useState() hook and react dev tools
    understood useState hook
    used useState() hook
    lnstalled react dev tool

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import "./Home.css"
 
 const Home = () => {
+    // we can assign new value to the variable but changes will not affect on the webpages
+    // we can change the state of the variable using useState hook
+    // const [varibale, functionToSetVariable] = useEffect(initial value)
+
+    const [name,setName] = useState('rohit')
 
     const handleClick=()=>{
-        console.log('i am clicked');
+        setName('mothit')
     }
     return ( 
         <div className="Home">
             <h1>homepage</h1>
+            <p>{name}</p>
             <button onClick={handleClick}>click me</button>
         </div>
      );
