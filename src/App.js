38fbc387ca3ca27d2ae1fba7ac9277291674226c
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2022-11-10 13:51:18 +0530

    lesson-4
    learnt about
    dynamic values in template
    multiple values in template
    expression in template
    cannot directly display object
    use dynamic value in the attributes of elements tag

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "welcome to the new blog"
+  const likes = 50
+
+  const person = 
+  {
+    name : "rohit",
+    age : 19
+  }
+  const link = "https://facebook.com"
+
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +29,29 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+      <div className="content">
+        {/* dynamic values in component */}
+        <h1>{title}</h1>
+        <p>liked {likes} times</p>
+        {/* connot directly display object */}
+        {/* <p>{person}</p> */}
+        <p>Name : {person.name}, Age : {person.age}</p>
+
+        {/* output multiple values */}
+        {/* values */}
+        <p>{15}</p>
+        {/* string */}
+        <p>{"this is a string"}</p>
+        {/* expressions */}
+        <p>{Math.random() *5}</p>
+
+        {/* dynamic values in attributes */}
+        <a href={link}>go to facebook</a>
+
+
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
