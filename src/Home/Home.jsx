1728584d14a8242fdfe4ab77b266bf3d958c87b0
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2022-11-10 18:14:10 +0530

    reused component and used filter method
    used filter method and reused the component

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -19,12 +19,13 @@ const Home = () => {
   
     const [blogs,setBlog] = useState([
         {title:"my new website", preview:"lorem epsum...",author:"mario",id:1},
-        {title:"this is unreal", preview:"lorem epsum...",author:"mario",id:2},
+        {title:"this is unreal", preview:"lorem epsum...",author:"rohit",id:2},
         {title:"react js is awsome", preview:"lorem epsum...",author:"mario",id:3},
     ])
     return ( 
         <div className="Home">
           <BlogList blogs={blogs} title="All blogs"/>
+          <BlogList blogs={blogs.filter((blog)=>blog.author === 'mario')} title="Mario's blogs"/>
         </div>
      );
 }
