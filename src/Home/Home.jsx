574f946e20b1f5a975a8cc1824e00dc1ddb38320
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2022-11-11 15:58:55 +0530

    function as a props
    passed funciton as a prop and used filter method

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./Home.css"
 
-const BlogList = ({blogs,title}) => {
+const BlogList = ({blogs,title,handleDelete}) => {
     return ( 
         <div className="blogList">
             <h2 className="title">{title}</h2>
@@ -9,6 +9,7 @@ const BlogList = ({blogs,title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={()=>handleDelete(blog.id)}>delete</button>
                 </div>
             ))}
         </div>
@@ -22,9 +23,14 @@ const Home = () => {
         {title:"this is unreal", preview:"lorem epsum...",author:"rohit",id:2},
         {title:"react js is awsome", preview:"lorem epsum...",author:"mario",id:3},
     ])
+
+    const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog =>blog.id !== id)
+        setBlog(newBlogs)
+    }
     return ( 
         <div className="Home">
-          <BlogList blogs={blogs} title="All blogs"/>
+          <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
           <BlogList blogs={blogs.filter((blog)=>blog.author === 'mario')} title="Mario's blogs"/>
         </div>
      );
