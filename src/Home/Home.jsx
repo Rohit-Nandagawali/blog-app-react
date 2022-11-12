110f76322cceb5063e13573efdd73cde9cc4d7a7
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2022-11-12 16:27:28 +0530

    created custome hook as useFetch
    created custome hook as useFetch()

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useState } from "react";
+
+import useFetch from "../useFetch";
 import "./Home.css"
 
 const BlogList = ({blogs,title,handleDelete}) => {
@@ -19,45 +19,20 @@ const BlogList = ({blogs,title,handleDelete}) => {
  
 const Home = () => {
   
-    const [blogs,setBlog] = useState(null)
-    const [isPending,setIsPending] = useState(true)
-    const [error,setError] = useState(null)
+    const {data : blogs,isPending,error} = useFetch("http://localhost:8000/blogs")
 
     const handleDelete = (id) =>{
         const newBlogs = blogs.filter(blog =>blog.id !== id)
-        setBlog(newBlogs)
+        // setBlog(newBlogs)
     }
 
-    // called everytime when the state changes
-    // we can also set to run useEffect hook when specific state is changes [name]
-    // if it is set to [] empty dependecies then useEffect will be called on the first render
-    useEffect(()=>{
-            fetch("http://localhost:8000/blogs") //fetch info from this and then
-            .then(responce =>{              //then get a responce
-                if(!responce.ok){           //if status is not ok
-                    throw Error("Could not fetch data for that resource")
-                }
-                return responce.json()      //convert that json file into javascript object
-            })
-            .then(
-                data =>{
-                    setError(null)
-                    setIsPending(false)
-                    setBlog(data)
-                }
-            )
-            .catch(err => {
-                setIsPending(false)
-                setError(err.message);   //if there is any error occur while connecting to server
-            })
-    },[]) //this will be called only once and first time
-
+    
 
     return ( 
         <div className="Home">
             {error && <div className="text-color">{error}</div>} 
             {isPending && <div className="text-color">Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
+            {blogs && <BlogList blogs={blogs} title="All blogs" />}
             {/* <BlogList blogs={blogs.filter((blog)=>blog.author === 'mario')} title="Mario's blogs"/> */}
         </div>
      );
