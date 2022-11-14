ccc390ccd2253917a313a1de4983d8a6c4f855e0
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2022-11-14 16:04:10 +0530

    deleting a blog by making DELETE request
    deleting a blog by making DELETE request

--- src/BlogDetails/BlogDetails.jsx
+++ src/BlogDetails/BlogDetails.jsx
@@ -1,9 +1,21 @@
-import { useParams } from "react-router-dom";
+import { useParams,useHistory } from "react-router-dom";
 import useFetch from "../useFetch";
+
 import './BlogDetails.css'
 const BlogDetails = () => {
     const {id} = useParams()
+    const history = useHistory()
     const {data : blog,isPending,error} = useFetch(`http://localhost:8000/blogs/${id}`)
+
+    const handleDelete = () =>{
+        fetch(`http://localhost:8000/blogs/${blog.id}`,{
+            method:"DELETE"
+        })
+        .then(()=>{
+            history.push('/')
+        })
+    }
+
     return ( 
         <div className="BlogDetails">
                 {error && <div >{error}</div>} 
@@ -13,6 +25,7 @@ const BlogDetails = () => {
                         <h2>{blog.title}</h2>
                         <p>written by - {blog.author}</p>
                         <div>{blog.body}</div>
+                        <button onClick={handleDelete}>Delete</button>
                     </article>
                 )}
                 
